6ff652e290c30adcbda663854dc98a20c1c7c0da
GOJO5551 <ivan_lisint15@hotmail.com>
2024-04-30 00:58:04 -0500

    proyecto

---
+++ javascript/javascript.js
@@ -0,0 +1,23 @@
+// Función para manejar la compra de productos
+function comprarProducto(idProducto) {
+  // Aquí se agrega la lógica para añadir el producto 
+  alert('¡Gracias por tu compra del producto ' + idProducto + '!');
+}
+
+// Función para mostrar la ubicación en el mapa
+function mostrarUbicacion() {
+  // Aquí se agrega la lógica para mostrar la ubicación de la tienda
+  alert('Nuestra tienda está ubicada en Calle Huamboya & Buenavista.');
+}
+
+// Eventos para los botones de compra
+document.addEventListener('DOMContentLoaded', function() {
+  var botonesCompra = document.querySelectorAll('button');
+  botonesCompra.forEach(function(boton, index) {
+
+    if(boton.getAttribute('onclick') === 'mostrarUbicacion()') return;
+    boton.addEventListener('click', function() {
+      comprarProducto(index + 1);
+    });
+  });
+})
